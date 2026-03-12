2e27cbc797bc436bcab3cd243fc82997a09c8ea4
JuanPabloSR <juanpablosanchezrey22@gmail.com>
2026-03-12 18:56:09 -0500

    Checkpoint from VS Code for cloud agent session

--- .angular/cache/19.2.12/advicx-ng/vite/deps/ngx-slick-carousel.js
+++ .angular/cache/19.2.12/advicx-ng/vite/deps/ngx-slick-carousel.js
@@ -27,8 +27,8 @@ import {
   ɵɵprojection,
   ɵɵprojectionDef
 } from "./chunk-II576EUL.js";
-import "./chunk-PEBH6BBU.js";
 import "./chunk-WPM5VTLQ.js";
+import "./chunk-PEBH6BBU.js";
 import "./chunk-4S3KYZTJ.js";
 import "./chunk-3OV72XIM.js";
 
