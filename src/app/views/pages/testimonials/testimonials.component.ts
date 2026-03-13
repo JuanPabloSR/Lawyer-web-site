8f35a5376e1aee59444c358171f1f824e0265b42
JuanPabloSR <juanpablosanchezrey22@gmail.com>
2026-03-12 19:11:03 -0500

    refactor: Remove unused pagination component

--- src/app/views/pages/testimonials/testimonials.component.ts
+++ src/app/views/pages/testimonials/testimonials.component.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { PageHeaderComponent } from '@app/components/page-header/page-header.component';
 import { TestimonialSwiperComponent } from "@app/components/testimonial-swiper/testimonial-swiper.component";
-import { PaginationComponent } from "@app/components/pagination/pagination.component";
 
 @Component({
     selector: 'app-testimonials',
-    imports: [PageHeaderComponent, TestimonialSwiperComponent, CommonModule, PaginationComponent],
+    imports: [PageHeaderComponent, TestimonialSwiperComponent, CommonModule],
     templateUrl: './testimonials.component.html',
     styles: ``
 })
