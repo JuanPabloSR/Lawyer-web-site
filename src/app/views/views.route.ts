bac845c8e4739486f20cd254e5c6c8cadb0d7f2c
JuanPabloSR <juanpablosanchezrey22@gmail.com>
2026-06-20 17:47:31 -0500

    feat: initialize Lighthouse reporting, configure app routing, and add global base styles

--- src/app/views/views.route.ts
+++ src/app/views/views.route.ts
@@ -15,5 +15,15 @@ export const VIEWS_ROUTES: Routes = [
         path: '',
         component: LayoutComponent,
         loadChildren: () => import('./services/services.route').then((mod) => mod.SERVICES_ROUTES)
+    },
+    {
+        path: '',
+        component: LayoutComponent,
+        loadChildren: () => import('./case-studies/case-study.route').then((mod) => mod.CASE_STUDY_ROUTES)
+    },
+    {
+        path: '',
+        component: LayoutComponent,
+        loadChildren: () => import('./blogs/blogs.route').then((mod) => mod.BLOGS_ROUTES)
     }
 ];
